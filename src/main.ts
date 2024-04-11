570ebe66e2c8282c465b0b6d46f3c900608f8203
kesumen <kesumen_2000@mail.ru>
2024-04-12 01:10:30 +0200

    working variant

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,38 @@ import { join } from 'path';
 import * as cookieParser from 'cookie-parser';
 import * as fs from 'fs';
 import * as enforce from 'express-sslify';
+import * as http from 'http';
+import * as https from 'https';
 
 async function bootstrap() {
-  //const keyFile = fs.readFileSync(__dirname + '/../ssl/private.key');
-  //const certFile = fs.readFileSync(__dirname + '/../ssl/certificate.crt');
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule /*, {
+  const keyFile = fs.readFileSync(__dirname + '/../ssl/private.key');
+  const certFile = fs.readFileSync(__dirname + '/../ssl/certificate.crt');
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     httpsOptions: {
       key: keyFile,
       cert: certFile,
     },
-  }*/,
-  );
-  //app.use(enforce.HTTPS());
+  });
+
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.enableCors();
   app.use(cookieParser());
   app.useStaticAssets(join(__dirname, '..', 'public'));
+
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
   await app.listen(443);
+
+  const httpServer = http.createServer((req, res) => {
+    res.writeHead(301, { Location: 'https://' + req.headers.host + req.url });
+    res.end();
+  });
+
+  httpServer.listen(80, () => {
+    console.log('HTTP server running on port 80');
+  });
 }
+
 bootstrap();
